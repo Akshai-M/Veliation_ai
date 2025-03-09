1f3a760dab638479e852b807831d5ec14ea69c77
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-03-09 16:55:02 +0530

    Myapp

---
+++ src/app/Myapp.jsx
@@ -0,0 +1,12 @@
+import Chatbot from './Chatbot';
+
+function Myapp() {
+  return (
+    <div className='inline-block'>
+      {/* <Dragdrop /> */}
+     <Chatbot/>
+    </div>
+  );
+}
+
+export default Myapp;
